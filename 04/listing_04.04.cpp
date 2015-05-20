5813b84f73816552f8722bd091db27ed3d87cef6
John Plaice <cartesianprogramming@gmail.com>
2015-05-20 18:24:16 -0400

    Listing 4.4 now compiles completely. 	modified:   listing_04.04.cpp

--- 04/listing_04.04.cpp
+++ 04/listing_04.04.cpp
@@ -1,5 +1,31 @@
 #include "listing_04.04.h"
 
+data_chunk prepare_data()
+{
+    data_chunk chunk;
+    return chunk;
+}
+
+bool more_data_to_prepare()
+{
+    static int i=100;
+    if (--i)
+        return true;
+    return false;
+}
+
+void process(data_chunk chunk)
+{
+}
+
+bool is_last_chunk(data_chunk)
+{
+    static int i=100;
+    if (--i)
+        return false;
+    return true;
+}
+
 int main()
 {
     return 0;
