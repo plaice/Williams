3ab099c84275e590dcdcebe4da92b20577a08cb1
John Plaice <cartesianprogramming@gmail.com>
2015-05-20 20:19:20 -0400

    Added Listing 7.12. 	new file:   lf_queue_rc_broken.h

---
+++ 07/lf_queue_rc_broken.h
@@ -0,0 +1,26 @@
+// Listing 14, p.212, Williams.
+// A (broken) first attempt at revising push().
+
+#include <memory>
+
+template <typename T>
+void push(T new_value)
+{
+    std::unique_ptr<T> new_data(new T(new_value));
+    counted_node_ptr new_next;
+    new_next.ptr=new node;
+    new_next.external_count=1;
+    for(;;)
+    {
+        node* const old_tail=tail.load();
+        T* old_data=nullptr;
+        if(old_tail->data.compare_exchange_strong(
+               old_data,new_data.get()))
+        {
+            old_tail->next=new_next;
+            tail.store(new_next.ptr);
+            new_data.release();
+            break;
+        }
+    }
+}
