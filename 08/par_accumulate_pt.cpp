b58605f7d4d1a3dd9a562dee84b90f4c66af4a78
John Plaice <cartesianprogramming@gmail.com>
2015-05-21 10:34:50 -0400

    Listing 8.3 compiles. 	modified:   .gitignore 	modified:   Makefile 	modified:   par_accumulate_pt.cpp 	modified:   par_accumulate_pt.h

--- 08/par_accumulate_pt.cpp
+++ 08/par_accumulate_pt.cpp
@@ -1,8 +1,9 @@
 #include "par_accumulate_pt.h"
+#include <list>
 
 int main (int argc, char* argv[])
 {
-   std::vector<int> input;
+   std::list<int> input;
    int result = parallel_accumulate(input.begin(), input.end(), 0);
    return 0;
 }
